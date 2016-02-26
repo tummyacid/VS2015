55e2913bc7bc7aeed008efe0d6ba3bf3234ff58e
Dave Burkett <Dave.Burkett@cutterbuck.com>
2016-02-25 17:54:18 -0800

    Added Frinkiac client

---
+++ FrinkiacClient/Frinkiac.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrinkiacClient
+{
+    class Frinkiac
+    {
+    }
+
+    public class Search
+    {
+        [JsonProperty("Id")]
+        public String Id;
+        [JsonProperty("Episode")]
+        public String Episode;
+        [JsonProperty("Timestamp")]
+        public String Timestamp;
+        [JsonProperty("Filename")]
+        public String Filename;
+    }
+
+    public class Frame
+    {
+        public int Id { get; set; }
+        public string Episode { get; set; }
+        public int Timestamp { get; set; }
+        public string Filename { get; set; }
+    }
+
+    public class Subtitle
+    {
+        public int Id { get; set; }
+        public string Episode { get; set; }
+        public int StartTimestamp { get; set; }
+        public int EndTimestamp { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class Nearby
+    {
+        public int Id { get; set; }
+        public string Episode { get; set; }
+        public int Timestamp { get; set; }
+        public string Filename { get; set; }
+    }
+
+    public class CaptionQuery
+    {
+        public Frame Frame { get; set; }
+        public List<Subtitle> Subtitles { get; set; }
+        public List<Nearby> Nearby { get; set; }
+    }
+}
---
+++ FrinkiacClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using ImageMagick;
+using System.Collections.Specialized;
+using System.Xml.Linq;
+
+namespace FrinkiacClient
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string searchQuery = System.Windows.Forms.Clipboard.GetText();
+            using (MagickImageCollection collection = new MagickImageCollection())
+            using (WebClient wc = new WebClient())
+            {
+                List<Search> target = JsonConvert.DeserializeObject<List<Search>>(wc.DownloadString("https://frinkiac.com/api/search?q=" + searchQuery.Replace(" ", "%20")));
+                CaptionQuery cq = JsonConvert.DeserializeObject<CaptionQuery>(wc.DownloadString("https://frinkiac.com/api/caption?e=" + target[0].Episode + "&t=" + target[0].Timestamp));
+                foreach (Nearby frame in cq.Nearby)
+                {
+                    collection.Add(new MagickImage(wc.DownloadData("https://frinkiac.com/img/" + frame.Episode + "/" + frame.Timestamp + ".jpg"), new MagickReadSettings()));
+                    collection[collection.Count - 1].AnimationDelay = 20;
+                }
+
+                // Optionally reduce colors
+                QuantizeSettings settings = new QuantizeSettings();
+                settings.Colors = 256;
+                collection.Quantize(settings);
+
+                // Optionally optimize the images (images should have the same size).
+                collection.Optimize();
+
+                //Upload gif to imgur
+                string clientID = "key";
+                wc.Headers.Add("Authorization", "Client-ID " + clientID);
+                NameValueCollection values = new NameValueCollection
+                    {
+                        { "image", Convert.ToBase64String(collection.ToByteArray(MagickFormat.Gif)) }
+                    };
+
+                XDocument reply = XDocument.Load(new MemoryStream(wc.UploadValues("https://api.imgur.com/3/upload.xml", values)));
+                System.Console.WriteLine(reply.Root.Element("link"));
+                System.Windows.Forms.Clipboard.SetText(reply.Root.Element("link").Value + " <--- " + searchQuery);
+
+            }
+            
+        }
+    }
+}
